f341dd71ab0107e576e61f8a41d6f103da54178e
ubeyid oumer <ubeyidah@gmail.com>
2025-07-12 14:52:02 +0300

    feat: create env function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("envguard")
+export const env = (name: string, alt: any = undefined) => {
+  const envValue = process.env[name] || alt;
+  if (!envValue) {
+    throw new Error(`Environment variable ${name} is not set`);
+  }
+  return envValue;
+};
