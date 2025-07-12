4dd0df200df761fc515692151211d746d10a5e19
ubeyid oumer <ubeyidah@gmail.com>
2025-07-12 15:01:28 +0300

    feat: add test  for env function wtih vitest

---
+++ test/index.test.ts
@@ -0,0 +1,19 @@
+import {describe, it, expect} from "vitest";
+import {env} from "../src/index";
+
+describe('env()', () => { 
+    it("retrns env value", () => {
+        process.env.TEST_ENV = "envgaurd";
+        expect(env("TEST_ENV")).toBe("envgaurd");
+    });
+
+    it("returns fallback if env is not set", () => {
+        delete process.env.TEST_ENV;
+        expect(env("TEST_ENV","default")).toBe("default");
+    });
+
+    it("throws if env is not set and no fallback", () => {
+        delete process.env.MISSING_ENV;
+        expect(()=> env("MISSING_ENV")).toThrow();
+    } )
+ })
