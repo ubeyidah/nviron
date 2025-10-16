7c9ba619eeec790f25a29a7f781336a62f156693
ubeyidah <ubeyidah@gmail.com>
2025-10-16 10:46:44 +0300

    chore(web): remove apps/web, create /docs, setup Tailwind CSS & shadcn/ui

--- apps/web/app/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>NVIRON</div>;
-};
-
-export default Page;
---
+++ docs/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div>
+      NVIRON
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Page;
