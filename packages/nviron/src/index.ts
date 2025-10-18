cb7c88a39af21f91d7b3f7466aeb8d104aa2db62
ubeyidah <ubeyidah@gmail.com>
2025-10-18 22:53:43 +0300

    chore(types): export EnvConfig, EnvSchema, ValidatedEnv and re-export Zod for better DX

--- packages/nviron/src/index.ts
+++ packages/nviron/src/index.ts
@@ -3,3 +3,7 @@ import z from "zod";
 
 export default defineEnv;
 export { defineEnv, z };
+
+// type export
+export type { EnvConfig, ValidatedEnv } from "./types";
+export type { EnvSchema } from "./types/env-schema";
--- packages/nviron/src/types/index.ts
+++ packages/nviron/src/types/index.ts
@@ -1,5 +1,10 @@
+import z from "zod";
+import { EnvSchema } from "./env-schema";
+
 export type EnvData = Record<string, unknown>;
 export interface EnvConfig {
   source?: EnvData;
   prefix?: string;
 }
+
+export type ValidatedEnv<T extends EnvSchema> = z.infer<z.ZodObject<T>>;
